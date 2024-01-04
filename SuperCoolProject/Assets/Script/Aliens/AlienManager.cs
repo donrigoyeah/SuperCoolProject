787ad50010d2916803b75077924afc0a21db9774
donrigoyeah <46565056+donrigoyeah@users.noreply.github.com>
2024-01-04 14:03:10 +0100

    Fixed PauseMenu Stats

--- SuperCoolProject/Assets/Script/Aliens/AlienManager.cs
+++ SuperCoolProject/Assets/Script/Aliens/AlienManager.cs
@@ -16,7 +16,6 @@ public class AlienManager : MonoBehaviour
     public int totalActiveAliens;
 
     public Image[] imagesPieChart;
-    public Slider[] menuResourcesSlider;
     public float[] values;
 
     [Header("Spawn Settings")]
@@ -124,7 +123,6 @@ public class AlienManager : MonoBehaviour
         {
             totalValues += FindPercentage(valuesToSet, i);
             imagesPieChart[i].fillAmount = totalValues;
-            menuResourcesSlider[i].value = totalValues;
         }
     }
 
